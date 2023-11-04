eb36b9290f8b6094e284e30e45f289032f5ab9cd
Sheylon <sheylo24@gmail.com>
2023-11-04 12:53:30 -0500

    cambios

--- modelo/dao/conexion_be.php
+++ modelo/dao/conexion_be.php
@@ -2,13 +2,6 @@
 
 $conexion = mysqli_connect("localhost", "root", "", "login_register_db");
 
-
-// if ($conexion) {
-// echo 'Conectado exitosamente a la Base de Datos';
-// }else{
-// echo 'No se ha podido conectar a la Base de Datos';
-// }
-
 ?>
 
 
