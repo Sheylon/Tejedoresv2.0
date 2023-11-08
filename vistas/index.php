eba793e88e6826136561d7450b349475a0bdcd54
yeimercampo <141436206+yeimercampo@users.noreply.github.com>
2023-11-08 16:55:39 -0500

    cambio usuario

--- vistas/index.php
+++ vistas/index.php
@@ -24,53 +24,25 @@ if(isset($_SESSION['$NOMBRE_USUARIO'])){
             <ul>
                 <li>
                     <ul class="menu">
-                        <li>
-                            <a href="">
-                                <div class="profile-dropdown-btn">
-                                    <div class="profile-img">
-                                    </div>
-                                    <span><?php
-                                        echo $_SESSION ['$NOMBRE_USUARIO'];
-                                        ?>
-                                        <i class="fa-solid fa-angle-down"></i>
-                                    </span>
-                            </a>
-                            <ul class="submenu2">
-                                <li>
-                                    <a clas="enlace_seccion" href="funciones/carrito/index.html">
-                                        <i class="fa-solid fa-cart-shopping"></i>
-                                        Compras</a>
-                                </li>
-                                <li>
-                                    <a clas="enlace_seccion" href="funciones/Vender/index.html">
-                                        <i class="fa-solid fa-chart-line"></i>
-                                        Vender</a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <i class="fa-regular fa-user"></i>
-                                        Editar Perfil
-                                    </a>
-                                </li>
-                                
-                                <li><a href="#">
-                                    <i class="fa-solid fa-sliders"></i>
-                                    configuración</a>
-                                </li>
-                                <li><a href="#">
-                                    <i class="fa-regular fa-circle-question"></i>
-                                    Servicio de soporte</a>
-                                </li>
-                                <hr />
-                                <li><a href="../controlador/action/act_lagout.php"><i class="fa-solid fa-arrow-right-from-bracket"></i>
-                                    Cerrar sesión</a>
-                                </li>
-                            </ul>
-                        </li>
+                    <li>
+                        <?php
+                        if (isset($_SESSION['NOMBRE_USUARIO'])) {
+                            echo '<a href="#"><div class="profile-dropdown-btn"><div class="profile-img"></div><span>' . $_SESSION['NOMBRE_USUARIO'] . '<i class="fa-solid fa-angle-down"></i></span></a>';
+                            echo '<ul class="submenu2">';
+                            echo '<li><a clas="enlace_seccion" href="funciones/carrito/index.html"><i class="fa-solid fa-cart-shopping"></i> Compras</a></li>';
+                            echo '<li><a clas="enlace_seccion" href="funciones/Vender/index.html"><i class="fa-solid fa-chart-line"></i> Vender</a></li>';
+                            echo '<li><a href="./perfil.php"><i class="fa-regular fa-user"></i> Perfil</a></li>';
+                            echo '<li><a href="#"><i class="fa-solid fa-sliders"></i> Configuración</a></li>';
+                            echo '<li><a href="#"><i class="fa-regular fa-circle-question"></i> Servicio de soporte</a></li>';
+                            echo '<hr />';
+                            echo '<li><a href="../controlador/action/act_lagout.php"><i class="fa-solid fa-arrow-right-from-bracket"></i> Cerrar sesión</a></li>';
+                            echo '</ul>';
+                        } else {
+                            echo '<a href="RegistroLogin.php" class="enlace_seccion">Inicio | Registro</a>';
+                        }
+                        ?>
+                    </li>
                     </ul>
-                </li>
-                <li>
-                    <a clas="enlace_seccion" href="RegistroLogin.php">Inicio | Registro </a>
                 </li>
             </ul>
         </div>
