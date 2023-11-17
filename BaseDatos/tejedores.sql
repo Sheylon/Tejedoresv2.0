1e9f51923210ebbdf7d809c48e31a71fea0c66c0
Sheylon <sheylo24@gmail.com>
2023-11-17 15:31:58 -0500

    SSS

--- BaseDatos/tejedores.sql
+++ BaseDatos/tejedores.sql
@@ -1,23 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 17-11-2023 a las 18:17:55
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
 -- Base de datos: `tejedores`
 --
 
@@ -152,10 +133,6 @@ INSERT INTO `usuario` (`idusuario`, `nombre_completo`, `correo`, `usuario`, `rol
 (3, 'camilo lopez', 'camilo@gmail.com', 'camilo', 'usuario', '1234');
 
 --
--- Índices para tablas volcadas
---
-
---
 -- Indices de la tabla `carrito`
 --
 ALTER TABLE `carrito`
@@ -165,13 +142,13 @@ ALTER TABLE `carrito`
 --
 -- Indices de la tabla `categoriaproducto`
 --
-ALTER TABLE `categoriaproducto`
+  ALTER TABLE `categoriaproducto`
   ADD PRIMARY KEY (`idcategoriaproducto`);
 
 --
 -- Indices de la tabla `detallecarrito`
 --
-ALTER TABLE `detallecarrito`
+ ALTER TABLE `detallecarrito`
   ADD PRIMARY KEY (`iddetallecarrito`),
   ADD KEY `detallecarrito_fk0` (`idcarrito`),
   ADD KEY `detallecarrito_fk1` (`idproducto`);
@@ -328,6 +305,3 @@ ALTER TABLE `producto`
   ADD CONSTRAINT `producto_fk0` FOREIGN KEY (`idcategoriaproducto`) REFERENCES `categoriaproducto` (`idcategoriaproducto`);
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
