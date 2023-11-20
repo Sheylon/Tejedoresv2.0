a844fc1d7a0b131bec24016e5326c29db77fbf06
yeimercampo <141436206+yeimercampo@users.noreply.github.com>
2023-11-20 13:30:41 -0500

    rij

--- controlador/action/act_registrarProducto.php
+++ controlador/action/act_registrarProducto.php
@@ -5,10 +5,9 @@ session_start();
 require_once(__DIR__ . "/../../modelo/entidad/Producto.php");
 require_once(__DIR__ . "/../mdb/mdbProductos.php");
 
-if (
-    isset($_POST['nombre'], $_POST['descripcion'], $_POST['unidades_disponibles'],
-     $_POST['valor_unidad'], $_POST['idcategoriaproducto'],  $_POST['id_talla'],  $_POST['id_usuario'])
-) {
+if (isset($_POST['nombre'], $_POST['descripcion'], $_POST['unidades_disponibles'],
+    $_POST['valor_unidad'], $_POST['idcategoriaproducto'],  $_POST['id_talla'],  
+    $_POST['id_usuario'])) {
     $nombreProducto = $_POST['nombre'];
     $descripcion = $_POST['descripcion'];
     $unidadesDisponibles = $_POST['unidades_disponibles'];
@@ -17,16 +16,16 @@ if (
     $idTalla = $_POST['id_talla'];
     $idusuario = $_POST['id_usuario'];
 
-   
+    
     $producto = new Producto( null, $nombreProducto, $descripcion, $unidadesDisponibles, $valorUnidad, $idTipoProducto, $idTalla, $idusuario);
 
     $resultado = insertarProducto($producto);
 
     if ($resultado != 0) {
-       
+    
         header("Location:../../vistas/index.php?msg=Producto registrado exitosamente");
     } else {
-       
+    
         header("Location: ../../vista/editarPerfil.php?msg=Error en el registro del producto");
     }
 } else {
