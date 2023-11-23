88d80beeb582767e295beaf7a1a25bdfcd16ae95
Sheylon <sheylo24@gmail.com>
2023-11-23 09:28:43 -0500

    ss

--- controlador/action/act_registrarProducto.php
+++ controlador/action/act_registrarProducto.php
@@ -5,11 +5,12 @@ session_start();
 require_once(__DIR__ . "/../../modelo/entidad/Producto.php");
 require_once(__DIR__ . "/../../modelo/entidad/Foto.php");
 require_once(__DIR__ . "/../mdb/mdbProductos.php");
-require_once(__DIR__ . "/../mdb/mdbFoto.php");
+require_once(__DIR__ . "/../mdb/mdbFotos.php");
+
 
 
 if (isset($_POST['nombre'], $_POST['descripcion'], $_POST['unidades_disponibles'],
-    $_POST['valor_unidad'], $_POST['idcategoriaproducto'],  $_POST['id_talla'])) {
+    $_POST['valor_unidad'], $_POST['idcategoriaproducto'], $_POST['id_talla'])) {
 
     $nombreProducto = $_POST['nombre'];
     $descripcion = $_POST['descripcion'];
@@ -17,73 +18,66 @@ if (isset($_POST['nombre'], $_POST['descripcion'], $_POST['unidades_disponibles'
     $valorUnidad = $_POST['valor_unidad'];
     $idTipoProducto = $_POST['idcategoriaproducto'];
     $idTalla = $_POST['id_talla'];
-    $idusuario =  $_SESSION['ID_USUARIO'];
-    
-    $producto = new Producto( null, $nombreProducto, $descripcion, $unidadesDisponibles, $valorUnidad, $idTipoProducto, $idTalla, $idusuario);
+    $idusuario = $_SESSION['ID_USUARIO'];
 
-    $resultado = insertarProducto($producto);
+    $producto = new Producto(null, $nombreProducto, $descripcion, $unidadesDisponibles, $valorUnidad, $idTipoProducto, $idTalla, $idusuario);
 
-    if ($resultado != 0) {
+    $resultadoProducto = insertarProducto($producto);
 
+    if ($resultadoProducto) {
         $idProducto = buscarProductoPorNombre($nombreProducto);
-        $urlFOto = generarURL();
+        $urlFoto = generarURL();
 
-        $foto = new Foto(NULL, "sssssurlFOto",$idProducto);
+        if ($urlFoto) {
+            $foto = new Foto(null, $urlFoto, 1);
+            $resultadoFoto = insertarFoto($foto);
 
-        insertarFoto($foto);
-
-    }
-
-
-    if ($resultado) {
-    
-        header("Location:../../vistas/index.php?msg=Producto registrado exitosamente");
-    } else {
-    
-        header("Location: ../../vista/editarPerfil.php?msg=Error en el registro del producto");
-    }
-} else {
-    
-    // header("Location: ../../vista/RegistroProducto.php?msg=Datos de registro incompletos");
-}
-
-
-
-function generarURL(){
-       
-    if (isset($_FILES['image']) && $_FILES['image']['error'] === UPLOAD_ERR_OK) {
-        $nombreArchivo = $_FILES['image']['name'];
-        $tipoArchivo = $_FILES['image']['type'];
-        $archivoTemporal = $_FILES['image']['tmp_name'];
-
-        $directorioDestino = __DIR__ . "/../../Fotos"; 
-        // Genera un nombre de archivo único para evitar sobrescribir archivos existentes
-        $archivoDestino = $directorioDestino . uniqid() . '/' . basename($nombreArchivo);
-        
-        // Verifica si el archivo tiene un formato de imagen válido
-        $tipoArchivo = strtolower(pathinfo($archivoDestino, PATHINFO_EXTENSION));
-        $formatosPermitidos = array("jpg", "jpeg", "png");
-        
-        if (in_array($tipoArchivo, $formatosPermitidos)) {
-            // Mueve el archivo subido al directorio de destino
-            if (move_uploaded_file($archivoTemporal, $archivoDestino)) {
-                // Subida de archivo exitosa.
-                return $archivoDestino;
-                header("Location: ../../vistas/index.php?msg=Foto subida exitosamente");
+            if ($resultadoFoto) {
+                header("Location:../../vistas/index.php?msg=Producto registrado exitosamente");
                 exit();
             } else {
-                // Error al mover el archivo, redirige con un mensaje de error
-                header("Location: ../../vistas/RegistroProducto.php?msg=Error al subir la foto");
+                header("Location:../../vistas/RegistroProducto.php?msg=Error al subir la foto");
                 exit();
             }
         } else {
-            // Formato de archivo no válido, redirige con un mensaje de error
-            header("Location: ../../vistas/RegistroProducto.php?msg=Formato de archivo no válido");
+            header("Location:../../vistas/RegistroProducto.php?msg=Error al subir la foto (URL vacía)");
             exit();
         }
-    }  
-              
+    } else {
+        header("Location: ../../vista/editarPerfil.php?msg=Error en el registro del producto");
+        exit();
+    }
+} else {
+    header("Location: ../../vista/RegistroProducto.php?msg=Datos de registro incompletos");
+    exit();
 }
 
+function generarURL()
+{
+    if (isset($_FILES['image']) && $_FILES['image']['error'] === UPLOAD_ERR_OK) {
+        $nombreArchivo = $_FILES['image']['name'];
+        $archivoTemporal = $_FILES['image']['tmp_name'];
 
+        $directorioDestino = __DIR__ . "/../../Fotos/";
+        $archivoDestino = $directorioDestino . uniqid() . '_' . basename($nombreArchivo);
+
+        $tipoArchivo = strtolower(pathinfo($archivoDestino, PATHINFO_EXTENSION));
+        $formatosPermitidos = array("jpg", "jpeg", "png");
+
+        if (in_array($tipoArchivo, $formatosPermitidos)) {
+            if (move_uploaded_file($archivoTemporal, $archivoDestino)) {
+                return $archivoDestino;
+            } else {
+                // Puedes lanzar una excepción en caso de error
+                throw new Exception("Error al mover el archivo");
+            }
+        } else {
+            // Puedes lanzar una excepción en caso de formato no válido
+            throw new Exception("Formato de archivo no válido");
+        }
+    } else {
+        // Puedes lanzar una excepción en caso de que no se haya seleccionado ningún archivo
+        throw new Exception("No se seleccionó ningún archivo");
+    }
+}
 ?>
