0e4b3d1c5388bf885677f8f9031b1057dc639bbf
Sheylon <sheylo24@gmail.com>
2023-11-22 20:41:40 -0500

    ssss

--- vistas/MostrasProductos.php
+++ vistas/MostrasProductos.php
@@ -15,8 +15,7 @@ if (isset($_SESSION['NOMBRE_USUARIO'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MostrasProductos</title> 
     <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="stylesheet" href="css/stylePagHistoria.css">
-    <link rel="stylesheet" href="css/card.css">
+    <link rel="stylesheet" href="css/styleMostrasProductos.css">
     <link rel="stylesheet" href="css/styleHeaderFooter.css">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"/>
@@ -32,41 +31,43 @@ if (isset($_SESSION['NOMBRE_USUARIO'])) {
         ?>
     </header>
     
-<div class="container-title">Ropa</div>
+    <div class="container-title">Ropa</div>
 
-<?php
-    $productoDAO = new ProductoDAO(); // Asegúrate de crear una instancia de ProductoDAO
-    $listproducto = $productoDAO->leerProductos();
+    <?php
+        $productoDAO = new ProductoDAO(); // Asegúrate de crear una instancia de ProductoDAO
+        $listproducto = $productoDAO->leerProductos();
     ?>
-
-<?php foreach ($listproducto as $Producto) { ?>
     
-    <div class="card-container">
-        <div class="item-a">
-            <div class="box">
-                <div class="slide-img">
-                    <img src="img/blusa-2.jpg" alt="" />
-                    <div class="overlay">
-                        <a href="#" class="buy-btn">Buy Now</a>
+    <div class="container">
+        <?php foreach ($listproducto as $Producto) { ?>
+        
+            <div class="card-container">
+                <div class="item-a">
+                    <div class="box">
+                        <div class="slide-img">
+                            <img src="img/blusa-2.jpg" alt="" />
+                            <div class="overlay">
+                                <a href="#" class="buy-btn">Buy Now</a>
+                            </div>
+                        </div>
+                    
+                        <div class="detail-box">
+                            <div class="type">
+                                <a><?php echo $Producto->getNombre(); ?></a>
+                            </div>
+                            <a class="price"><?php echo $Producto->getValorUnidad(); ?></a>
+                        </div>
                     </div>
-                </div>
-              
-                <div class="detail-box">
-                    <div class="type">
-                        <a><?php echo $Producto->getNombre(); ?></a>
-                    </div>
-                    <a class="price"><?php echo $Producto->getValorUnidad(); ?></a>
                 </div>
             </div>
-        </div>
+        
+        <?php } ?>
     </div>
-
-<?php } ?>
 
     <footer>
         <?php
         require_once("./Templates/Footer.php");
-     ?>
+         ?>
     </footer>
 
 <body> 
